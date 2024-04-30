be5e778b0cb1b6677a5f3f544caf59748afaa73c
Filip Ratajczak <90644163+Tesla2000@users.noreply.github.com>
2024-04-30 12:34:19 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 class Config:
     root = Path(__file__).parent
+
+for variable in dir(Config):
+    value = getattr(Config, variable)
+    if isinstance(value, Path) and value.is_dir():
+        value.mkdir(parents=True, exist_ok=True)

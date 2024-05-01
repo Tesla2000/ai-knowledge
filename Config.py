3bc2511ea39e3eb9ee9b63175bf0ad75449aa442
Filip Ratajczak <90644163+Tesla2000@users.noreply.github.com>
2024-05-01 21:23:52 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -10,5 +10,5 @@ class Config:
 
 for variable in dir(Config):
     value = getattr(Config, variable)
-    if isinstance(value, Path) and value.suffix == "":
-        value.mkdir(parents=True, exist_ok=True)
+    if isinstance(value, Path) and value.suffix == "" and not value.exists():
+        value.mkdir(parents=True)

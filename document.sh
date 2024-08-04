06d4181a22556d177c344d53a589278bd0f4fca2
Tesla2000 <fratajczak124@gmail.com>
2024-08-04 13:42:23 +0200

    Updated document.sh

--- document.sh
+++ document.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-# Default values if arguments are not provided
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --markdown-docs-path)
+            MARKDOWN_DOCS_PATH="$2"
+            shift 2
+            ;;
+        --language)
+            LANGUAGE="$2"
+            shift 2
+            ;;
+        --target-repo-path)
+            TARGET_REPO_PATH="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
 MARKDOWN_DOCS_PATH="${MARKDOWN_DOCS_PATH:-docs}"
 LANGUAGE="${LANGUAGE:-English}"
 TARGET_REPO_PATH="${TARGET_REPO_PATH:-$(pwd)}"

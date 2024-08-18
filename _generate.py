3cd3ff2b15c710d45947a97a3ad2462f80972a88
Tesla2000 <fratajczak124@gmail.com>
2024-08-18 18:50:31 +0200

    Added files to main

--- _generate.py
+++ _generate.py
@@ -11,6 +11,7 @@ def generate_modifications(project_path: Path) -> dict[str, str]:
     project_name = project_path.name
     return {
         "project_name": project_name,
+        "project_name_low": project_name.lower(),
         "description": input("Provide project description"),
         "script_name": project_name.replace("_", "-"),
         "docker_image_name": project_name,
